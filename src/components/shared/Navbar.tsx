031c5de39319413fc851f023b9e268ceefc13c6a
Sudipta Das <shiningsudipto@gmail.com>
2024-08-16 17:46:48 +0600

    sub menu bar added

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -26,6 +26,14 @@ import {
   servicesMenu,
 } from "@/utils/menuLinks";
 
+import { RiFacebookCircleLine } from "react-icons/ri";
+import { TiSocialTwitterCircular } from "react-icons/ti";
+import { FaRegCirclePlay } from "react-icons/fa6";
+
+import { MdOutlineDarkMode } from "react-icons/md";
+import { MdOutlineLightMode } from "react-icons/md";
+import { useState } from "react";
+
 const menuLinks = [
   {
     title: "About us",
@@ -167,110 +175,158 @@ const menuLinks = [
 ];
 
 const Navbar = () => {
+  const [darkMode, setDarkMode] = useState(false);
+  const [isEnglish, setEnglish] = useState(false);
   return (
-    <div className="w-full h-[70px] bg-primary-foreground/10 text-black flex items-center justify-between md:px-[70px] px-5">
-      <p className="font-bold text-2xl">
-        <span className="text-primary">ICT</span>{" "}
-        <span className="text-primary-foreground">Division</span>
-      </p>
-      <div>
-        <div className="lg:hidden block">
-          <Drawer direction="right">
-            <DrawerTrigger>
-              <IoMenu className="text-2xl" />{" "}
-            </DrawerTrigger>
-            <DrawerContent className="">
-              <DrawerClose className="flex justify-end m-2">
-                <AiOutlineCloseSquare className="text-3xl p-1" />
-              </DrawerClose>
-              <div className="flex flex-col w-[250px] gap-y-3 font-medium px-4 max-h-screen overflow-y-scroll">
-                {menuLinks.map((menu, menuIndex) => (
-                  <Accordion
-                    key={menuIndex}
-                    type="single"
-                    collapsible
-                    className="w-full"
-                  >
-                    <AccordionItem value={`item-${menuIndex}`} className="">
-                      <AccordionTrigger className="h-[40px] text-start">
-                        {menu.title}
-                      </AccordionTrigger>
-                      <AccordionContent>
-                        {menu.subMenus.map((subMenu, subMenuIndex) => (
-                          <div key={subMenuIndex} className="pl-4">
-                            {subMenu.title ? (
-                              <Accordion type="single" collapsible>
-                                <AccordionItem
-                                  value={`subItem-${subMenuIndex}`}
-                                >
-                                  <AccordionTrigger className="text-start">
-                                    {subMenu.title}
-                                  </AccordionTrigger>
-                                  <AccordionContent>
-                                    <ul>
-                                      {subMenu.links.map((link, linkIndex) => (
-                                        <li key={linkIndex}>
-                                          <Link
-                                            to={link.href}
-                                            className="block py-1"
-                                          >
-                                            {link.label}
-                                          </Link>
-                                        </li>
-                                      ))}
-                                    </ul>
-                                  </AccordionContent>
-                                </AccordionItem>
-                              </Accordion>
-                            ) : (
-                              <ul>
-                                {subMenu.links.map((link, linkIndex) => (
-                                  <li key={linkIndex}>
-                                    <Link to={link.href} className="block py-1">
-                                      {link.label}
-                                    </Link>
-                                  </li>
-                                ))}
-                              </ul>
-                            )}
-                          </div>
-                        ))}
-                      </AccordionContent>
-                    </AccordionItem>
-                  </Accordion>
-                ))}
-              </div>
-            </DrawerContent>
-          </Drawer>
+    <>
+      <div className="h-[30px] flex items-center justify-between bg-primary-foreground text-white md:px-[70px] px-5">
+        <div className="flex items-center gap-x-2 text-white">
+          <RiFacebookCircleLine className="text-[20px] cursor-pointer hover:text-primary" />{" "}
+          <TiSocialTwitterCircular className="text-[23px] cursor-pointer hover:text-primary" />{" "}
+          <FaRegCirclePlay className="text-[17px] cursor-pointer hover:text-primary" />
         </div>
-        <ul className="items-center gap-x-3 hidden lg:flex">
-          <li>
-            <CustomMenu options={aboutUs} label="About us" />
-          </li>
-          <li>
-            <CustomMenu options={institutions} label="Institutions" />
-          </li>
-          <li>
-            <CustomMenu options={projectMenu} label="Projects" />
-          </li>
-          <li>
-            <CustomMenu options={galleryMenu} label="Gallery" />
-          </li>
-          <li>
-            <CustomMenu
-              options={contactAndCommentMenu}
-              label="Contact & Comment"
-            />
-          </li>
-          <li>
-            <CustomMenu options={policyMenu} label="Policy" />
-          </li>
-          <li>
-            <CustomMenu options={servicesMenu} label="e-Services" />
-          </li>
-        </ul>
+        <div className="flex items-center gap-x-3">
+          <div>
+            {darkMode ? (
+              <MdOutlineLightMode
+                onClick={() => setDarkMode(!darkMode)}
+                className="text-[20px] cursor-pointer hover:text-primary"
+              />
+            ) : (
+              <MdOutlineDarkMode
+                onClick={() => setDarkMode(!darkMode)}
+                className="text-[20px] cursor-pointer hover:text-primary"
+              />
+            )}
+          </div>
+          <div>
+            {isEnglish ? (
+              <p
+                onClick={() => setEnglish(!isEnglish)}
+                className="cursor-pointer hover:text-primary"
+              >
+                বাংলা
+              </p>
+            ) : (
+              <p
+                onClick={() => setEnglish(!isEnglish)}
+                className="cursor-pointer hover:text-primary"
+              >
+                English
+              </p>
+            )}
+          </div>
+        </div>
       </div>
-    </div>
+      <div className="w-full h-[70px] bg-primary-foreground/10 text-black flex items-center justify-between md:px-[70px] px-5">
+        <p className="font-bold text-2xl">
+          <span className="text-primary">ICT</span>{" "}
+          <span className="text-primary-foreground">Division</span>
+        </p>
+        <div>
+          <div className="lg:hidden block">
+            <Drawer direction="right">
+              <DrawerTrigger>
+                <IoMenu className="text-2xl" />{" "}
+              </DrawerTrigger>
+              <DrawerContent className="">
+                <DrawerClose className="flex justify-end m-2">
+                  <AiOutlineCloseSquare className="text-3xl p-1" />
+                </DrawerClose>
+                <div className="flex flex-col w-[250px] gap-y-3 font-medium px-4 max-h-screen overflow-y-scroll">
+                  {menuLinks.map((menu, menuIndex) => (
+                    <Accordion
+                      key={menuIndex}
+                      type="single"
+                      collapsible
+                      className="w-full"
+                    >
+                      <AccordionItem value={`item-${menuIndex}`} className="">
+                        <AccordionTrigger className="h-[40px] text-start">
+                          {menu.title}
+                        </AccordionTrigger>
+                        <AccordionContent>
+                          {menu.subMenus.map((subMenu, subMenuIndex) => (
+                            <div key={subMenuIndex} className="pl-4">
+                              {subMenu.title ? (
+                                <Accordion type="single" collapsible>
+                                  <AccordionItem
+                                    value={`subItem-${subMenuIndex}`}
+                                  >
+                                    <AccordionTrigger className="text-start">
+                                      {subMenu.title}
+                                    </AccordionTrigger>
+                                    <AccordionContent>
+                                      <ul>
+                                        {subMenu.links.map(
+                                          (link, linkIndex) => (
+                                            <li key={linkIndex}>
+                                              <Link
+                                                to={link.href}
+                                                className="block py-1"
+                                              >
+                                                {link.label}
+                                              </Link>
+                                            </li>
+                                          )
+                                        )}
+                                      </ul>
+                                    </AccordionContent>
+                                  </AccordionItem>
+                                </Accordion>
+                              ) : (
+                                <ul>
+                                  {subMenu.links.map((link, linkIndex) => (
+                                    <li key={linkIndex}>
+                                      <Link
+                                        to={link.href}
+                                        className="block py-1"
+                                      >
+                                        {link.label}
+                                      </Link>
+                                    </li>
+                                  ))}
+                                </ul>
+                              )}
+                            </div>
+                          ))}
+                        </AccordionContent>
+                      </AccordionItem>
+                    </Accordion>
+                  ))}
+                </div>
+              </DrawerContent>
+            </Drawer>
+          </div>
+          <ul className="items-center gap-x-3 hidden lg:flex">
+            <li>
+              <CustomMenu options={aboutUs} label="About us" />
+            </li>
+            <li>
+              <CustomMenu options={institutions} label="Institutions" />
+            </li>
+            <li>
+              <CustomMenu options={projectMenu} label="Projects" />
+            </li>
+            <li>
+              <CustomMenu options={galleryMenu} label="Gallery" />
+            </li>
+            <li>
+              <CustomMenu
+                options={contactAndCommentMenu}
+                label="Contact & Comment"
+              />
+            </li>
+            <li>
+              <CustomMenu options={policyMenu} label="Policy" />
+            </li>
+            <li>
+              <CustomMenu options={servicesMenu} label="e-Services" />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
   );
 };
 
