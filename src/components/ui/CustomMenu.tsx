4c03bf33e4d503fe51f25755d9228cb65d72cc2b
Sudipta Das <shiningsudipto@gmail.com>
2024-08-16 14:32:19 +0600

    website width fixed

--- src/components/ui/CustomMenu.tsx
+++ src/components/ui/CustomMenu.tsx
@@ -20,7 +20,9 @@ const CustomMenu = ({ options, label }) => {
               {options?.map((section) => (
                 <li key={section.title} className="w-[150px]">
                   {section?.title && (
-                    <h3 className="text-sm font-semibold">{section.title}</h3>
+                    <h3 className="text-sm font-semibold text-primary-foreground">
+                      {section.title}
+                    </h3>
                   )}
                   <ul className="mt-1 space-y-1">
                     {section?.links?.map((link) => (
